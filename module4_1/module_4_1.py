dfe8830b7cfc5a224c01cbf0c86d7e1ac9cb743d
serhio4smotrov <ser19061995@gmail.com>
2024-09-30 16:48:56 +0300

    initial

--- module4_1/module_4_1.py
+++ module4_1/module_4_1.py
@@ -1,3 +1,3 @@
-from fake_math import divide as fake_div
-from true_math import divide as true_div
+from module4_1.fake_math import divide as fake_div
+from module4_1.true_math import divide as true_div
 print(f'Школьное деление {fake_div(24, 0)}\nДеление в ВУЗе {true_div(10,0)}')
